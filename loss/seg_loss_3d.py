e24944fc101ba702e6db43c73f00cf4e97f2b147
chao <cc7287@nyu.edu>
2024-11-15 11:16:48 -0500

    update r2

---
+++ loss/seg_loss_3d.py
@@ -0,0 +1,76 @@
+import torch
+import torch
+from torch import nn
+import torch.nn.functional as F
+import numpy as np
+import matplotlib.pyplot as plt
+
+class SegmentationLoss3D(nn.Module):
+    def __init__(self, class_weights):
+        super(SegmentationLoss3D, self).__init__()
+        self.ignore_index = 255
+        self.class_weights = class_weights
+
+    def forward(self, pred, target):
+        if target.shape[1] != 1:
+            raise ValueError('segmentation label must be index label with channel dim = 1')
+        
+        b, s, c, h, w, d = pred.shape
+        pred_seg = pred.view(b * s, c, h, w, d)
+        gt_seg = target.view(b * s, h, w, d)
+
+        # self.plot_grid_gt(target[0].clone().detach(), save_path="./visual/gt.png")
+
+        seg_loss = F.cross_entropy(pred_seg,
+                                   gt_seg,
+                                   reduction='none',
+                                   ignore_index=self.ignore_index,
+                                   weight=self.class_weights.to(gt_seg.device))
+
+        return torch.mean(seg_loss)
+
+    def plot_grid_gt(self, threeD_grid, save_path=None, vmax=None, layer=None):
+        threeD_grid = threeD_grid.squeeze(0)
+        H, W, D = threeD_grid.shape
+        threeD_grid[threeD_grid==14] = 0
+        threeD_grid[threeD_grid==4] = 1
+
+        twoD_map = threeD_grid.max(dim=2).values  # compress 3D to 2D, get the values
+        cmap = plt.cm.viridis  # viridis color projection
+        if twoD_map.is_cuda:
+            twoD_map = twoD_map.cpu().numpy()
+        else:
+            twoD_map = twoD_map.numpy()
+
+        if vmax is None:
+            vmax = np.max(twoD_map) * 1.2  
+        plt.imshow(twoD_map, cmap=cmap, origin='upper', vmin=np.min(twoD_map), vmax=vmax) # plot 2D
+
+        color_legend = plt.colorbar()
+        color_legend.set_label('Color Legend') # legend
+
+        if save_path:
+            plt.savefig(save_path)
+        else:
+            plt.show()
+        plt.close()
+
+    def plot_grid_pred(self, threeD_grid, save_path=None, vmax=None, layer=None):
+        threeD_grid = np.mean(threeD_grid,0)
+        H, W, D = threeD_grid.shape
+        twoD_map = np.max(threeD_grid, axis=2)# compress 3D-> 2D
+        # twoD_map = threeD_grid[:,:,7]
+        cmap = plt.cm.viridis # viridis color projection
+
+        if vmax is None:
+            vmax=np.max(twoD_map)*1.2
+        plt.imshow(twoD_map, cmap=cmap, origin='upper', vmin=np.min(twoD_map), vmax=vmax) # plot 2D
+
+        color_legend = plt.colorbar()
+        color_legend.set_label('Color Legend') # legend
+
+        if save_path:
+            plt.savefig(save_path)
+        else:
+            plt.show()
+        plt.close()
---
+++ trainer/pl_processor.py
@@ -0,0 +1,197 @@
+import torch
+import pytorch_lightning as pl
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+from pytorch_lightning.callbacks import ModelCheckpoint, TQDMProgressBar, LearningRateMonitor, ModelSummary
+from tool.config import Configuration
+from loss.control_loss import ControlLoss, ControlValLoss
+from loss.depth_loss import DepthLoss
+from loss.seg_loss import SegmentationLoss
+from loss.seg_loss_3d import SegmentationLoss3D
+from model.parking_model import ParkingModel
+from collections import OrderedDict
+
+import torch.nn.functional as F
+
+def setup_callbacks(cfg):
+    callbacks = []
+
+    ckpt_callback = ModelCheckpoint(dirpath=cfg.checkpoint_dir,
+                                    monitor='val_loss',
+                                    save_top_k=3,
+                                    mode='min',
+                                    filename='E2EParking-{epoch:02d}-{val_loss:.2f}',
+                                    save_last=True)
+    callbacks.append(ckpt_callback)
+
+    progress_bar = TQDMProgressBar()
+    callbacks.append(progress_bar)
+
+    model_summary = ModelSummary(max_depth=2)
+    callbacks.append(model_summary)
+
+    lr_monitor = LearningRateMonitor(logging_interval='epoch')
+    callbacks.append(lr_monitor)
+    return callbacks
+
+
+class ParkingProcessingModule(pl.LightningModule):
+    def __init__(self, cfg: Configuration):
+        super(ParkingProcessingModule, self).__init__()
+        self.save_hyperparameters()
+
+        self.cfg = cfg
+
+        # self.control_loss_func = ControlLoss(self.cfg)
+
+        # self.control_val_loss_func = ControlValLoss(self.cfg)
+
+        # # self.segmentation_loss_func_3D = SegmentationLoss3D(
+        # #     class_weights=torch.Tensor(self.cfg.seg_vehicle_weights)
+        # # )
+        # self.segmentation_loss_func = SegmentationLoss(
+        #     class_weights=torch.Tensor(self.cfg.seg_vehicle_weights)
+        # )
+
+        # self.depth_loss_func = DepthLoss(self.cfg)
+
+        self.parking_model = ParkingModel(self.cfg)
+        if cfg.model_path is not None:
+            ckpt = torch.load(cfg.model_path, map_location='cuda:0')
+            state_dict = OrderedDict([(k.replace('parking_model.', ''), v) for k, v in ckpt['state_dict'].items()])
+            self.parking_model.load_state_dict(state_dict)
+            self.parking_model.to('cuda:0')
+
+    def training_step(self, batch, batch_idx):
+        loss_dict = {}
+        # import pdb; pdb.set_trace()
+        # pred_c, pred_f, pred_depth = self.parking_model(batch)
+        # # pred_control, pred_segmentation, pred_depth = self.parking_model(batch)
+        # pred_control, pred_segmentation = self.parking_model(batch)
+        # control_loss = self.control_loss_func(pred_control, batch)
+        # loss_dict.update({
+        #     "control_loss": 10*control_loss
+        # })
+        
+        # # H, W, D = pred_f.shape[-3:]
+        # # pred_c = F.interpolate(pred_c, size=[H, W, D], mode='trilinear', align_corners=False).contiguous()
+        # # segmentation_loss_coarse = self.segmentation_loss_func_3D(pred_c.unsqueeze(1), batch['segmentation'])
+        # # loss_dict.update({
+        # #     "segmentation_loss_coarse": segmentation_loss_coarse
+        # # })
+        # segmentation_loss = self.segmentation_loss_func(pred_segmentation.unsqueeze(1), batch['segmentation'])
+        # loss_dict.update({
+        #     "segmentation_loss": segmentation_loss
+        # })
+
+        # # depth_loss = self.depth_loss_func(pred_depth, batch['depth'])
+        # # loss_dict.update({
+        # #     "depth_loss": depth_loss
+        # # })
+        loss_dict.update({
+            "train_loss": 0
+        })
+        train_loss = sum(loss_dict.values())
+
+        self.log_dict(loss_dict)
+        # self.log_segmentation(pred_segmentation, batch['segmentation'], 'segmentation')
+        # self.log_depth(pred_depth, batch['depth'], 'depth')
+
+        return train_loss
+
+    def validation_step(self, batch, batch_idx):
+        val_loss_dict = {}
+        # pred_control, pred_segmentation = self.parking_model(batch)
+
+        # acc_steer_val_loss, reverse_val_loss = self.control_val_loss_func(pred_control, batch)
+        # val_loss_dict.update({
+        #     "acc_steer_val_loss": acc_steer_val_loss,
+        #     "reverse_val_loss": reverse_val_loss
+        # })
+        # segmentation_val_loss = self.segmentation_loss_func(pred_segmentation.unsqueeze(1), batch['segmentation'])
+        # val_loss_dict.update({
+        #     "segmentation_val_loss": segmentation_val_loss
+        # })
+        # # H, W, D = pred_f.shape[-3:]
+        # # pred_c = F.interpolate(pred_c, size=[H, W, D], mode='trilinear', align_corners=False).contiguous()
+        # # segmentation_loss_coarse = self.segmentation_loss_func_3D(pred_c.unsqueeze(1), batch['segmentation'])
+        # # val_loss_dict.update({
+        # #     "segmentation_loss_coarse": segmentation_loss_coarse
+        # # })
+
+        # # segmentation_loss_fine = self.segmentation_loss_func_3D(pred_f.unsqueeze(1), batch['segmentation'])
+        # # val_loss_dict.update({
+        # #     "segmentation_loss_fine": segmentation_loss_fine
+        # # })
+
+        # # depth_val_loss = self.depth_loss_func(pred_depth, batch['depth'])
+        # # val_loss_dict.update({
+        # #     "depth_val_loss": depth_val_loss
+        # # })
+
+        val_loss_dict.update({
+            "val_loss": 0
+        })
+        val_loss = sum(val_loss_dict.values())
+
+        self.log_dict(val_loss_dict)
+        # self.log_segmentation(pred_segmentation, batch['segmentation'], 'segmentation_val')
+        # self.log_depth(pred_depth, batch['depth'], 'depth_val')
+
+        return val_loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(),
+                                     lr=self.cfg.learning_rate,
+                                     weight_decay=self.cfg.weight_decay)
+        lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer=optimizer, T_max=self.cfg.epochs)
+        return {"optimizer": optimizer, "lr_scheduler": lr_scheduler}
+
+    def log_segmentation(self, pred_segmentation, gt_segmentation, name):
+        fig, ax = plt.subplots(1, 2, figsize=(20, 10))
+        ax[0].set_title("GT Seg")
+        ax[1].set_title("Pred Seg")
+
+        pred_segmentation = pred_segmentation[0]
+        pred_segmentation = torch.argmax(pred_segmentation, dim=0, keepdim=True)
+        pred_segmentation = pred_segmentation.detach().cpu().numpy()
+        pred_segmentation[pred_segmentation == 1] = 128
+        pred_segmentation[pred_segmentation == 2] = 255
+        pred_seg_img = pred_segmentation[0, :, :][::-1]
+
+        gt_segmentation = gt_segmentation[0]
+        gt_segmentation = gt_segmentation.detach().cpu().numpy()
+        gt_segmentation[gt_segmentation == 1] = 128
+        gt_segmentation[gt_segmentation == 2] = 255
+        gt_seg_img = gt_segmentation[0, :, :][::-1]
+
+        norm = mpl.colors.Normalize(vmin=0, vmax=255)
+        ax[0].imshow(gt_seg_img, norm=norm)
+        ax[1].imshow(pred_seg_img, norm=norm)
+
+        tensorboard = self.logger.experiment
+        tensorboard.add_figure(figure=fig, tag=name)
+        plt.close(fig)
+
+    def log_depth(self, pred_depth, gt_depth, name):
+        fig, ax = plt.subplots(1, 2, figsize=(20, 10))
+        ax[0].set_title("GT Depth")
+        ax[1].set_title("Pred Depth")
+
+        pred_depth = pred_depth[1]
+        pred_depth = torch.argmax(pred_depth, dim=0)
+        pred_depth = pred_depth.detach().cpu().numpy()
+        pred_depth = pred_depth * self.cfg.d_bound[2] + self.cfg.d_bound[0]
+
+        gt_depth = gt_depth[0][1]
+        gt_depth = gt_depth.detach().cpu().numpy()
+
+        norm = mpl.colors.Normalize()
+        ax[0].imshow(gt_depth)
+        ax[1].imshow(pred_depth, norm=norm)
+
+        tensorboard = self.logger.experiment
+        tensorboard.add_figure(figure=fig, tag=name)
+        plt.close(fig)
+
