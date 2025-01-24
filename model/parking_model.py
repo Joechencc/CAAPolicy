a42916f48325cc101bea4ec1905831a5898fb71d
jas0n <sy3913@nyu.edu>
2025-01-23 19:10:35 -0500

    BUGFIX: concat 256+4(target point)+4(ego_motion)

--- model/parking_model.py
+++ model/parking_model.py
@@ -66,15 +66,17 @@ class ParkingModel(nn.Module):
         images = data['image'].to(self.cfg.device, non_blocking=True)
         intrinsics = data['intrinsics'].to(self.cfg.device, non_blocking=True)
         extrinsics = data['extrinsics'].to(self.cfg.device, non_blocking=True)
-        target_point = data['target_point'].to(self.cfg.device, non_blocking=True).unsqueeze(1)#已经是相对车的位置了
+        target_point = data['target_point'].to(self.cfg.device, non_blocking=True)#已经是相对车的位置了
         ego_motion = data['ego_motion'].to(self.cfg.device, non_blocking=True)
         bev_feature, pred_depth = self.bev_model(images, intrinsics, extrinsics)
 
         # bev_feature, bev_target = self.add_target_bev(bev_feature, target_point)
 
         bev_target = self.adjust_target_bev(bev_feature, target_point)
+
         bev_down_sample = self.bev_encoder(bev_feature)
 
+        target_point = target_point.unsqueeze(1)
         fuse_feature = self.feature_fusion(bev_down_sample, ego_motion, target_point)
 
         pred_segmentation = self.segmentation_head(fuse_feature)
