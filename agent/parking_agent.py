070f82ac652fe237ee1bebf3721acd54fa0f3d6e
YY-Splash <104396193+YY-Splash@users.noreply.github.com>
2025-07-06 15:03:02 -0400

    Update parking_agent.py

--- agent/parking_agent.py
+++ agent/parking_agent.py
@@ -33,17 +33,6 @@ except ImportError:
 from agent.speed_dynamics_model import SpeedDynamicsModel
 from agent.path_collector_TF_Dec_13 import VehiclePIDController
 
-
-def get_speed(vehicle):
-    """
-    Compute speed of a vehicle in Km/h.
-    :param vehicle: the vehicle for which speed is calculated
-    :return: speed as a float in Km/h
-    """
-    vel = vehicle.get_velocity()
-    return 3.6 * math.sqrt(vel.x ** 2 + vel.y ** 2 + vel.z ** 2)
-
-
 def analyze_and_split_path(path):
     """
     Analyze the path and split it into forward and reverse segments.
